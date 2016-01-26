1ce1799c25cfd4cd8d4633b519d1d2ef0d1f2229
KabirSingh114 <kabirs@umich.edu>
2016-01-26 02:27:34 -0500

    Update list.c

--- list.c
+++ list.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #include "list.h"
-#include < test> 
+
 // Takes a valid, sorted list starting at `head` and adds the element
 // `new_element` to the list. The list is sorted based on the value of the
 // `index` member in ascending order. Returns a pointer to the head of the list
@@ -10,7 +10,25 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
+	
+	list_t *current = head;
+	
+	if(head->index > new_element->index){
+		new_element->next = head;
+		return new_element;
+	}
 
+	while(1){
+		if(curr->next == NULL){
+			curr->next = new_element;
+			return head;
+		}else if(curr->next->index >= new_element->index){
+			new_element->next = curr->next;
+			curr->next = new_element;
+			return head;
+		}
+		curr = curr->next;
+	}
 	return head;   
 }
 
@@ -18,6 +36,22 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+    if(head == NULL || head->next == NULL)
+        return head;
+    
+    list_t *prev, *current, *nextNode;
+    prev = head;
+    current = head;
+    nextNode = head->next;
+    
+    while(nextNode!=NULL){
+        current = nextNode;
+        nextNode = nextNode->next;
+        current->next = prev;
+        prev = current;
+    }
+    head->next=NULL;
+head=prev;
 
 	return head;
 }
